7339d20ddc2455218ac5a102829d11fd783fefcf
KKD <keeyanakellydonaldson@Kianas-MacBook-Pro.local>
2019-11-04 14:46:13 -0800

    Refactor to be a class component

--- app.js
+++ app.js
@@ -7,24 +7,29 @@
 //     </ul>
 //   </div>
 // );
-var GroceryList = () => (
-  <div>
-    <GroceryListItem groceryItems={['apples', 'pears', 'peaches']}/>
-  </div>
-);
 
-const GroceryListItem = (props) => {
-  return (
-    <div>
-      <ul>
-        {props.groceryItems.map((item) => {
-          return (<li>{item}</li>)
-        })}
-      </ul>
-    </div>
-  )
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>{this.props.item}</li>
+    );
+  }
+
 }
 
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
 
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+ReactDOM.render(<GroceryList groceryItems={['apples', 'pears', 'peaches']}/>, document.getElementById("app"));
